8df59bfb8ba7fe9656af09c2d1e0e4b0d0b951b4
kaiy-d <kaiyuan624@gmail.com>
2016-06-04 21:46:04 +0300

    Create component TodoInput

---
+++ src/components/TodoInput.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+const styleTextField = {
+  margin: 20
+};
+
+export default class TodoInput extends Component {
+
+  state = {
+    inputValue: ''
+  }
+
+  onKeyDownInput = e => {
+    if (e.keyCode === 13) {
+      this.onAddTodoButtonClick(e);
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+
+  onAddTodoButtonClick = e => {
+    e.preventDefault();
+
+    if (this.state.inputValue.trim() === '') {
+      return;
+    }
+
+    this.props.addTodo(this.state.inputValue);
+    this.setState({ inputValue: '' });
+  }
+
+  render() {
+    return <div>
+      <TextField
+        onKeyDown={this.onKeyDownInput}
+        hintText='Enter what you need to do'
+        type='text'
+        value={this.state.inputValue}
+        style={styleTextField}
+        onChange={this.handleChange}
+      />
+      <RaisedButton label='Add todo' primary={true} onClick={this.onAddTodoButtonClick} />
+    </div>
+  }
+}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,54 +1,16 @@
 import React, { PropTypes, Component } from 'react';
-import ReactDOM from 'react-dom';
 import TodoItem from './TodoItem';
-
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-
-const styleTextField = {
-  margin: 20
-};
-
+import TodoInput from './TodoInput';
 
 //todos={todoList} addTodo={addTodo} deleteTodo={deleteTodo} toggleTodo={toggleTodo}
 
 export default class TodoList extends Component {
 
-  onKeyDownInput = e => {
-    if (e.keyCode === 13) {
-      this.onAddTodoButtonClick(e);
-    }
-  }
-
-  onAddTodoButtonClick = e => {
-    e.preventDefault();
-    let todoInput = this.refs['newTodo'];
-
-    console.log(todoInput);
-
-    // if (todoInput.getValue().trim() === '') return; 
-
-    this.props.addTodo(todoInput.getValue());
-    todoInput.setValue('');
-  }
-
-  componentDidMount() {
-    ReactDOM.findDOMNode(this.refs.newTodo).focus();
-  }
-
   render() {
-    const { todos, deleteTodo, toggleTodo } = this.props;
+    const { todos, deleteTodo, toggleTodo, addTodo } = this.props;
 
     return <div className='input-todo'>
-      <TextField
-        onKeyDown={this.onKeyDownInput}
-        hintText='Enter what you need to do'
-        type='text'
-        defaultValue=''
-        ref='newTodo'
-        style={styleTextField}
-      />
-      <RaisedButton label='Add todo' primary={true} onClick={this.onAddTodoButtonClick} />
+      <TodoInput addTodo={addTodo}/>
       <ul className='todos'>
         { todos.map((todo, index) => <TodoItem id={todo.id} 
           text={todo.text} 
